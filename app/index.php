60f508e8acfea16ee2e2c68b14dfa197e552b72e
Iago Sousa <iagosousa201486@gmail.com>
2024-05-05 00:23:09 -0300

    inicializando projeto

---
+++ app/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="resource/css/bootstrap.min.css">
+  <link rel="stylesheet" href="resource/css/style.css">
+  
+  <title>Gestor Manutenção</title>
+</head>
+<style>
+
+</style>
+<body>
+  <header class="headerDash"></header>
+  <section class="sidebarDash"></section>
+  <main class="mainDash">
+    <h2>Olá</h2>
+  </main>
+  
+
+  <script src="resource/js/jquery-3.7.1.min.js"></script>
+  <script src="resource/js/bootstrap.bundle.min.js"></script>
+  <script></script>
+</body>
+</html>
